fce1e8c36a6aea2f6e9a3b30e56c32bbf50a34d9
Max Kiss <maxkiss@ucsb.edu>
2019-01-12 16:55:37 -0800

    CSS and Styling

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/events')
+def events():
+    return render_template('events.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
