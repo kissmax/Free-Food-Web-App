d67330d3330724b75f54c65f14da8659462f4fa4
Max Kiss <maxkiss@ucsb.edu>
2019-01-13 23:43:44 -0800

    Added Events Table

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def events():
     return render_template('events.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
